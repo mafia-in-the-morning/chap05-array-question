922ca2b442165e3b1806c4c2dd473e3f8a785e4d
JulienJang <big00124@naver.com>
2023-05-08 16:30:56 +0900

    section01/array/level01/advanced Application1.java 해

--- src/main/java/com/ohgiraffers/section01/array/level04/advanced/Application1.java
+++ src/main/java/com/ohgiraffers/section01/array/level04/advanced/Application1.java
@@ -1,5 +1,8 @@
 package com.ohgiraffers.section01.array.level04.advanced;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Application1 {
 
 	public static void main(String[] args) {
@@ -9,6 +12,22 @@ public class Application1 {
 		 * 1부터 45까지의 중복되지 않는 난수를 발생시켜 각 인덱스에 대입한 뒤
 		 * 오름차순 정렬하여 출력하세요
 		 * */
+		int[] lotto = new int[6];
+		Random random = new Random();
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = random.nextInt(45) + 1; // 1부터 45까지의 난수 발생
+			for (int j = 0; j < i; j++) {
+				if (lotto[i] == lotto[j]) { // 중복되는 값이 있으면 다시 난수 발생
+					i--;
+					break;
+				}
+			}
+		}
+		Arrays.sort(lotto); // 오름차순 정렬
+
+		for (int i = 0; i < lotto.length; i++) {
+			System.out.print(lotto[i] + " ");
+		}
 
 	}
 
