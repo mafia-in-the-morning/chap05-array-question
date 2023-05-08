b05020a3c00d730e9d3e9e9ac6317e5836e97afe
JulienJang <big00124@naver.com>
2023-05-08 16:31:21 +0900

    section01/array/level01/advanced Application2.java 해

--- src/main/java/com/ohgiraffers/section01/array/level04/advanced/Application2.java
+++ src/main/java/com/ohgiraffers/section01/array/level04/advanced/Application2.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.section01.array.level04.advanced;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Application2 {
 
 	public static void main(String[] args) {
@@ -31,6 +32,76 @@ public class Application2 {
 		 * 4자리 숫자를 입력하세요 : 7416
 		 * 정답입니다.
 		 * */
+		int[] answer= generateAnswer();
+		int[] guess = new int[4];
+		int strike, ball;
+		int count = 0;
+		Scanner scanner = new Scanner(System.in);
+
+		while (count < 10) { // 10번의 기회가 있음
+			// 4자리 숫자 입력 받기
+			System.out.print("4자리 숫자를 입력하세요: ");
+			int num = scanner.nextInt();
+
+			if (num < 1000 || num > 9999) { // 4자리의 정수를 입력하지 않은 경우
+				System.out.println("4자리의 정수를 입력해야 합니다.");
+				continue; // 횟수 차감하지 않고 다시 입력 받음
+			}
+
+			// 입력 받은 숫자를 배열에 저장
+			for (int i = 3; i >= 0; i--) {
+				guess[i] = num % 10;
+				num /= 10;
+			}
+			strike=0;
+			ball=0;
+
+			for(int i=0; i<4; i++){
+				if(guess[i]==answer[i]){
+					strike++;
+				}
+				else if (contains(answer, guess[i])) { // 숫자는 맞지만 자리는 맞지 않는 경우
+					ball++;
+				}
+			}
+			// 결과 출력
+			if (strike == 4) { // 정답인 경우
+				System.out.println("정답입니다.");
+				break;
+			} else { // 오답인 경우
+				System.out.println(strike + "S " + ball + "B");
+				count++;
+			}
+		}
+
+		if (count == 10) { // 10번의 기회를 모두 소진한 경우
+			System.out.println("10번의 기회를 모두 소진하셨습니다. 프로그램을 종료합니다.");
+		}
+	}
+
+	// 0 ~ 9까지의 중복되지 않는 난수로 이루어진 길이 4의 정수 배열 생성하는 메소드
+	public static int[] generateAnswer() {
+		int[] arr = new int[4];
+		Random random = new Random();
+		for (int i = 0; i < 4; i++) {
+			int num;
+			do {
+				num = random.nextInt(10);
+			} while (contains(arr, num));
+			arr[i] = num;
+		}
+		return arr;
+	}
+
+	// 배열에 특정 값이 포함되어 있는지 확인하는 메소드
+	public static boolean contains(int[] arr, int num) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return true;
+			}
+		}
+		return false;
+
 
 	}
 
